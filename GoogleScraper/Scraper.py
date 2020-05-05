8fcc1ad4f909bcfa9a9bdb0155f143b45a1c332e
mpia3 <45994151+mpia3@users.noreply.github.com>
2020-05-05 20:20:24 +0200

    Update comments

--- GoogleScraper/Scraper.py
+++ GoogleScraper/Scraper.py
@@ -2,7 +2,9 @@ from google_images_download import google_images_download  # importing the libra
 
 response = google_images_download.googleimagesdownload()  # class instantiation
 
-arguments = {"keywords": "scarlett johansson,Megan Fox,Angelina Jolie", "limit": 20,
+#our keywords: Andrew Garfield 2019, Angelina Jolie 2019, Anthony Hopkins 2019, Ben Affleck 2019, Beyonce Knowles 2019
+
+arguments = {"keywords": "Andrew Garfield 2019, Angelina Jolie 2019, Anthony Hopkins 2019, Ben Affleck 2019, Beyonce Knowles 2019", "limit": 20,
              "print_urls": True}  # creating list of arguments
 paths = response.download(arguments)  # passing the arguments to the function
 print(paths)  # printing absolute paths of the downloaded images
