5409b0a5564f64cf2c6e712cbf7c4356eb08e101
mpia3 <45994151+mpia3@users.noreply.github.com>
2020-06-17 12:33:48 +0200

    Add run mode

--- main.py
+++ main.py
@@ -12,32 +12,42 @@
 from MaskDetection.actors_recognitionTEST import Single_test
 from MaskDetection.MaskCropper import cropEyeLineFromMasked
 from MaskDetection.keras_infer import inference
+from MaskDetection.keras_infer import run_on_video
 from PIL import Image
 import cv2
 
-actors = ['Andrew Garfield', 'Angelina Jolie', 'Anthony Hopkins', 'Ben Affleck',
-          'Beyonce Knowles']
+mode = 0
+video_path = ''
 
-actorsWMak = ['Andrew Garfield-Mask', 'Angelina Jolie-Mask', 'Anthony Hopkins-Mask', 'Ben Affleck-Mask',
-          'Beyonce Knowles-Mask']
+if __name__ == "__main__":
+    if mode == 0:
+        actors = ['Andrew Garfield', 'Angelina Jolie', 'Anthony Hopkins', 'Ben Affleck',
+                  'Beyonce Knowles']
 
-imageName = '3.jpg'
-original_img_path = 'H:\OLDSysAg\Datasets\ActorsWMask' + '/' + actorsWMak[1] + '/' + imageName
-original_img_path = 'H:\OLDSysAg\Datasets\SysAgDatasetFaceOnly\Test' + '/' + actors[1] + '/' + imageName
+        actorsWMak = ['Andrew Garfield-Mask', 'Angelina Jolie-Mask', 'Anthony Hopkins-Mask', 'Ben Affleck-Mask',
+                  'Beyonce Knowles-Mask']
 
-original_img = cv2.imread(original_img_path)
-original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
-Image.fromarray(original_img).show()
-checkMask = original_img.copy()
+        imageName = '3.jpg'
+        original_img_path = 'H:\OLDSysAg\Datasets\ActorsWMask' + '/' + actorsWMak[1] + '/' + imageName
+        original_img_path = 'H:\OLDSysAg\Datasets\SysAgDatasetFaceOnly\Test' + '/' + actors[1] + '/' + imageName
 
-output = inference(checkMask, show_result=True, target_shape=(260, 260))
-# print(output[0][0])
+        original_img = cv2.imread(original_img_path)
+        original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
+        Image.fromarray(original_img).show()
+        checkMask = original_img.copy()
 
-if output[0][0] == 0:   # Maschera identificata
-    print("Attore con maschera")
-    cropped_img = cropEyeLineFromMasked(original_img)
-    Image.fromarray(cropped_img).show()
-    Single_test(original_img, cropped_img)
-else:   # Maschera non identificata
-    print("Attore senza maschera")
-    Single_test(original_img, "X")
+        output = inference(checkMask, show_result=True, target_shape=(260, 260))
+        # print(output[0][0])
+
+        if output[0][0] == 0:   # Maschera identificata
+            print("Attore con maschera")
+            cropped_img = cropEyeLineFromMasked(original_img)
+            Image.fromarray(cropped_img).show()
+            Single_test(original_img, cropped_img)
+        else:   # Maschera non identificata
+            print("Attore senza maschera")
+            Single_test(original_img, "X")
+    elif mode == 1:
+        run_on_video(0, '', conf_thresh=0.5)
+    else:
+        run_on_video(video_path, '', conf_thresh=0.5)
